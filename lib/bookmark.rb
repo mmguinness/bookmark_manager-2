fff85c7c0f0c18318681f477cb13c2c83454c0f7
Marie Mc Guinness <mcguinnessmarie@gmail.com>
2022-02-17 16:34:51 +0000

    Wrapping returned data in instance of bookmark class

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,18 +1,24 @@
 require 'pg'
 
 class Bookmark 
-    
+  attr_reader :id, :title, :url
+
+	def initialize(id:, title:, url:)
+		@id = id
+		@title = title
+		@url = url
+	end
+
   def self.all
-		
 		if ENV['RACK_ENV'] == 'test'
 			conn = PG.connect( dbname: 'bookmark_manager_test' ) 
 		else
     	conn = PG.connect( dbname: 'bookmark_manager' ) 
 		end
-
-    rs = conn.exec "SELECT * FROM bookmarks"
-    rs.map { |bookmark| bookmark['url'] }
-
+    result = conn.exec("SELECT * FROM bookmarks")
+    result.map do |bookmark| 
+			Bookmark.new(id: bookmark['id'], title: bookmark['title'], url: bookmark['url'])
+		end
   end
 
 	def self.create(url, title)
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,17 +3,20 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
-
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+      
       # Add the test data
-      Bookmark.create('http://www.makersacademy.com', 'makers website')
-      Bookmark.create('http://www.google.com', 'google website')
+      bookmark = Bookmark.create(url: "http://www.makersacademy.com", title: "Makers Academy")
+      Bookmark.create(url: "http://www.destroyallsoftware.com", title: "Destroy All Software")
+      Bookmark.create(url: "http://www.google.com", title: "Google")
 
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.google.com")
-      expect(bookmarks).to include("makers website")
-      expect(bookmarks).to include("google website")
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers Academy'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 
